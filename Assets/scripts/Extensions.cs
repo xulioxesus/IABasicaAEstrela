482ee15c6ace395effd5f1199f81ffde5c865b9c
Xulio Xesús León Pérez <xulioxesus@gmail.com>
2024-11-21 11:48:42 +0100

    Reorganizando código

--- Assets/scripts/Extensions.cs
+++ Assets/scripts/Extensions.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public static class Extensions
 {
     private static System.Random rng = new System.Random();
+    /// <summary>
+    /// Método para mezclar os elementos dunha Lista
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="list"></param>
     public static void Shuffle<T>(this IList<T> list)
     {
         int n = list.Count;
